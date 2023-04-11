0cea405681fe41098bdac4b3059c75a0cdf9f2ea
Arjan <116271036+arjanrosero64@users.noreply.github.com>
2023-04-11 13:39:39 +0200

    commit numero 2

---
+++ src/src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/IClientes2.java
@@ -0,0 +1,18 @@
+package org.iesalandalus.programacion.alquilervehiculos;
+
+public interface IClientes {
+
+	public comenzar() {
+		return 
+	}
+		
+	public void terminar();
+	
+	
+	public list[] get(){
+		
+	}
+	
+	
+	
+}
